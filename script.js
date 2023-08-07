5a4ab638b101ca9abb0b735ada5de3e244c1838b
Predrag <predrag.bulatovic@seznam.cz>
2023-08-07 11:34:00 +0200

    little changes in code and functions

--- script.js
+++ script.js
@@ -1,50 +1,34 @@
 let options = ["rock", "paper", "scissors"];
 
-const computerPlay = () => {
+function computerPlay() {
   return options[Math.floor(Math.random() * options.length)];
 };
 
-const playRound = (playerSelection, computerSelection) => {
-  let gameBegin = confirm("let's play Rock, Paper, Scissors!");
+function playRound(playerSelection, computerSelection) {
+  let gameBegin = confirm("Let's play Rock, Paper, Scissors!");
   if (gameBegin) {
-    playerSelection = prompt("please enter rock, paper or scissors!");
+    playerSelection = prompt("Please enter rock, paper or scissors!");
 
-    if (playerSelection) {
-      playerSelection = playerSelection.trim().toLowerCase();
-      if (
-        playerSelection === "rock" ||
-        playerSelection === "paper" ||
-        playerSelection === "scissors"
-      ) {
-        let result =
-          playerSelection === computerSelection
-            ? `You played: ${playerSelection}, computer played: ${computerSelection}. It's a draw!`
-            : playerSelection === "paper" && computerSelection === "rock"
-            ? "You win! Paper beats Rock."
-            : playerSelection === "scissors" && computerSelection === "paper"
-            ? "You win! Scissors beats Paper."
-            : playerSelection === "rock" && computerSelection === "scissors"
-            ? "You win! Rock beats Scissors!"
-            : "You Lose!"; // we need more logic here for when computer wins
-        return result;
-      } else {
-        return "Incorrect entry!, Please try again...";
-      }
+    if (playerSelection === computerSelection) {
+      return playerSelection = prompt("It's a tie!");
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      return playerSelection = prompt("Cingrats! You win! Let's play next round. :) ");
     } else {
-      return "See you soon!";
+      return playerSelection = prompt("You lose! Try again :) ");
     }
-  } else {
-    return "Goodbye!";
-  }
 };
-
+}
 const playerSelection = "rock";
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection)); //used console to
 // test, we will use return later as per the instructions
 
 // Next we have to create a game() function.... and add a loop
-const game = () => {
+function game() {
   for (let i = 0; i < 5; i++) {
     //
   }
