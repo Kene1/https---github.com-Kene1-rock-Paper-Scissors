e2f539b01b0eb88a6bed6d385da8aab73e73fa5e
Predrag <predrag.bulatovic@seznam.cz>
2023-08-07 14:50:57 +0200

    other variant of code

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
-let options = ["rock", "paper", "scissors"];
-
+let gameBegin = confirm("Let's play Rock, Paper, Scissors!");
+if (gameBegin) {
+	prompt("Please enter rock, paper or scissors!");
+}
 function computerPlay() {
-  return options[Math.floor(Math.random() * options.length)];
-};
+	// Generate a random number between 0 and 2 (inclusive)
+	const randomNumber = Math.floor(Math.random() * 3);
+  
+	// Map the random number to 'Rock', 'Paper', or 'Scissors'
+	switch (randomNumber) {
+	  case 0:
+		return 'Rock';
+	  case 1:
+		return 'Paper';
+	  case 2:
+		return 'Scissors';
+	  default:
+	}
+}
 
 function playRound(playerSelection, computerSelection) {
-  let gameBegin = confirm("Let's play Rock, Paper, Scissors!");
-  if (gameBegin) {
-    playerSelection = prompt("Please enter rock, paper or scissors!");
 
-    if (playerSelection === computerSelection) {
-      return playerSelection = prompt("It's a tie!");
-    } else if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-      return playerSelection = prompt("Cingrats! You win! Let's play next round. :) ");
-    } else {
-      return playerSelection = prompt("You lose! Try again :) ");
-    }
-};
-}
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection)); //used console to
-// test, we will use return later as per the instructions
+	playerSelection = playerSelection.toLowerCase()
+	computerSelection = computerSelection.toLowerCase()
 
-// Next we have to create a game() function.... and add a loop
-function game() {
-  for (let i = 0; i < 5; i++) {
-    //
-  }
-};
+		// Check for a tie
+		if (playerSelection === computerSelection) {
+			return prompt("It's a tie!");
+		}
+  
+		// Check for the winner based on player's choice
+		switch (playerSelection) {
+			case "rock":
+				return prompt(computerSelection === "scissors" ? "You win! Rock beats Scissors." : "You lose! Paper beats Rock. Try again.");
+			case "paper":
+				return prompt(computerSelection === "rock" ? "You win! Paper beats Rock." : "You lose! Scissors beats Paper. Try again.");
+			case "scissors":
+				return prompt(computerSelection === "paper" ? "You win! Scissors beats Paper." : "You lose! Rock beats Scissors. Try again.");
+			default:
+				return prompt("Nice try :). Please choose 'Rock', 'Paper', or 'Scissors'.");
+		}
+	}
+	function game() {
+		for (let playerScore = 0; playerScore < 50; playerScore++) {
+			const playerSelection = "rock";
+			const computerSelection = computerPlay();
+			const result = playRound(playerSelection, computerSelection);
+
+			if (result === "You win!") {
+				playerScore++;
+			  } else if (result === "You lose!") {
+				return prompt("You lost! Try again :). ");
+			}
+		}
+	  };
+game();
+
+
